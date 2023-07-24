90053ce0ea775e5ecdf4962da5ed0d34df66647f
Pratik Mondal <pratikmondal6@hotmail.com>
2023-07-24 12:14:18 +0200

    integration test added--junit

--- src/main/java/com/product/productservice/repository/ProductRepository.java
+++ src/main/java/com/product/productservice/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.product.productservice.repository;
 
 import com.product.productservice.entity.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
 
 
